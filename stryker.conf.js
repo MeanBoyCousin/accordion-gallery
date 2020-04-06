86d5f0616e32a5ba4c6b8debd1465b5900d3b131
TimDunphy <timothy.dunphy@gmail.com>
2020-04-06 13:11:52 +0100

    NPM security updates. Updated jest/stryker and reconfigured.

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,19 +1,22 @@
-module.exports = function (config) {
-  config.set({
-    files: ['test/*.js'],
-    mutate: ['test/accordion-gallery.js'],
-    mutator: "javascript",
-    packageManager: "npm",
-    reporters: ["html", "clear-text", "progress", "dashboard"],
-    testRunner: "jest",
-    transpilers: [],
-    coverageAnalysis: "off",
-    timeoutMS: 10000,
-    dashboard: {
-      project: 'github.com/MeanBoyCousin/accordion-gallery',
-      version: 'master',
-      baseUrl: 'https://dashboard.stryker-mutator.io/api/reports',
-      reportType: 'full'
-    }
-  });
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  files: ['test/*.js'],
+  mutate: ['test/accordion-gallery.js'],
+  mutator: "javascript",
+  packageManager: "npm",
+  reporters: ["html", "clear-text", "progress", "dashboard"],
+  testRunner: "jest",
+  "tempDirName": "stryker-temp",
+  transpilers: [],
+  coverageAnalysis: "off",
+  timeoutMS: 10000,
+  maxConcurrentTestRunners: 2,
+  dashboard: {
+    project: 'github.com/MeanBoyCousin/accordion-gallery',
+    version: 'master',
+    baseUrl: 'https://dashboard.stryker-mutator.io/api/reports',
+    reportType: 'full'
+  }
 };
